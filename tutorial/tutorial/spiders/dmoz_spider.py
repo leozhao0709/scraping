b586ae6dd09f177652937e3c5c1bd0b36307d4e0
leozhao0709 <zhao434@usc.edu>
2016-05-07 00:26:54 -0700

    initial scrape

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+
+__author__ = 'lzhao'
+__date__ = '5/6/16'
+__time__ = '11:49 PM'
+
+
+class DmozSpider(scrapy.Spider):
+	name = "dmoz"
+	allowed_domains = ["dmoz.org"]
+	start_urls = [
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+	]
+
+	def parse(self, response):
+		filename = response.url.split("/")[-2] + '.html'
+		with open(filename, 'wb') as f:
+			f.write(response.body)
