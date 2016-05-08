8fa2e6291538df23f47c67442d0623b480aa3115
leozhao0709 <zhao434@usc.edu>
2016-05-08 01:02:13 -0700

    tutorial

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -6,15 +6,44 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
 
 class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	pass
 
 
-class DemozItem(scrapy.Item):
-    title = scrapy.Field()
-    link = scrapy.Field()
-    desc = scrapy.Field()
+class DmozItem(scrapy.Item):
+	title = scrapy.Field()
+	link = scrapy.Field()
+	desc = scrapy.Field()
+
+
+def filter_price(value):
+	if value.isdigit():
+		return value
+
+
+class Product(scrapy.Item):
+	name = scrapy.Field(
+		input_processor=MapCompose(remove_tags),
+		output_processor=Join()
+	)
+	price = scrapy.Field(
+		input_processor=MapCompose(remove_tags, filter_price),
+		output_processor=TakeFirst()
+	)
+	stock = scrapy.Field()
+	last_update = scrapy.Field(serializer=str)
+
+
+class DiscountedProduct(Product):
+	discount_percent = scrapy.Field(serializer=str)
+	discount_expiration_date = scrapy.Field()
+
+
+class SpecificProduct(Product):
+	name = scrapy.Field(Product.fields['name'], serializer=str)
--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -2,6 +2,8 @@
 
 import scrapy
 
+from tutorial.items import DmozItem
+
 __author__ = 'lzhao'
 __date__ = '5/6/16'
 __time__ = '11:49 PM'
@@ -11,11 +13,20 @@ class DmozSpider(scrapy.Spider):
 	name = "dmoz"
 	allowed_domains = ["dmoz.org"]
 	start_urls = [
-		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-		"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+		# "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+		# "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/",
 	]
 
 	def parse(self, response):
-		filename = response.url.split("/")[-2] + '.html'
-		with open(filename, 'wb') as f:
-			f.write(response.body)
+		for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+			url = response.urljoin(href.extract())
+			yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+	def parse_dir_contents(self, response):
+		for selector in response.xpath('//ul/li'):
+			item = DmozItem()
+			item['title'] = selector.xpath('a/text()').extract()
+			item['link'] = selector.xpath('a/@href').extract()
+			item['desc'] = selector.xpath('text()').extract()
+			yield item
